b566ccf93733e386fb9ca8b2e922c6e1fc741af4
ChrisArtbn <christianartabansetiawan22@gmail.com>
2026-04-20 09:12:43 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int main() {
+    int N;
+    if (scanf("%d", &N) != 1) return 0;
+    
+    long long A[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%lld", &A[i]);
+    }
+    
+    for (int i = 0; i < N; i++) {
+        if (A[i] == -1) {
+            long long angka_selanjutnya = 0;
+            for (int j = i + 1; j < N; j++) {
+                if (A[j] != -1) {
+                    angka_selanjutnya = A[j];
+                    break;
+                }
+            }
+            if (i == 0) {
+                A[i] = angka_selanjutnya;
+            } else {
+                A[i] = (A[i-1] + angka_selanjutnya) / 2;
+            }
+        }
+    }
+    
+    printf("RECOVERED");
+    for (int i = 0; i < N; i++) {
+        printf(" %lld", A[i]);
+    }
+    printf("\n");
+    
+    long long sum_total = A[0];
+    long long sum_sekarang = A[0];
+    
+    for (int i = 1; i < N; i++) {
+        if (sum_sekarang < 0) {
+            sum_sekarang = A[i];
+        } else {
+            sum_sekarang += A[i];
+        }
+        if (sum_sekarang > sum_total) {
+            sum_total = sum_sekarang;
+        }
+    }
+    printf("MAX_SUM %lld\n", sum_total);
 }
